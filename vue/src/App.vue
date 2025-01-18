fb3938e51ae3e285cd6dc7775e534cd1c8c721f0
hashedtomato3 <82042586+hashedtomato3@users.noreply.github.com>
2025-01-18 20:47:41 +0900

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -110,7 +110,7 @@
                               d="M13.5 21H6V17H13.5C15.43 17 17 15.43 17 13.5S15.43 10 13.5 10H11V14L4 8L11 2V6H13.5C17.64 6 21 9.36 21 13.5S17.64 21 13.5 21Z" />
                           </svg>
                         </b-button>
-                        <p class="title is-4">[Rule] {{ rule.title }}</p>
+                        <p class="title is-4">[Rule] {{ selectedRule.title }}</p>
                       </div>
                       <!--- contents --->
                       <div>
